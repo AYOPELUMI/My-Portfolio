b6817ebc47b7350ad0527e9595d8bd1d58459312
ayopelumi <ayopelumi2014@gmail.com>
2024-01-31 12:57:40 -0800

    worked on section section

--- src/assets/components/SecondSection/SecondSection.jsx
+++ src/assets/components/SecondSection/SecondSection.jsx
@@ -9,8 +9,16 @@ export const SecondSection = () => {
                 <h1>Services & Offer</h1>
                 <h4>I design web apps, breathing life into ideas with frameworks.<br/>My passion lies in optimizing codes, good UI&UX experience<br/> From idea to launch, i cultivate all ideas into reality</h4>
             </div>
-            <div>
-
+            <div className='cardCtnr'>
+                <div className='card'>
+                    card 1
+                </div>
+                <div className='card '>
+                    card 2
+                </div>
+                <div className='card'>
+                    card 3
+                </div>
             </div>
         </section>
     )
